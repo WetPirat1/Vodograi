e9f1266a92a487f4a6899d149ed62024ac4deaa1
WetPirat1 <EvgenSemydosky@ukr.net>
2023-11-24 16:57:47 +0200

    popUp

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,14 +1,43 @@
 $(document).ready(function(){
-    // Скрыть PopUp при загрузке страницы    
+    //Скрыть PopUp при загрузке страницы    
     PopUpHide();
 });
-
-// Функция отображения PopUp
+//Функция отображения PopUp
 function PopUpShow(){
     $("#popup-service-card-1").show();
 }
-
-// Функция скрытия PopUp
+//Функция скрытия PopUp
 function PopUpHide(){
     $("#popup-service-card-1").hide();
+}
+
+
+
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide2();
+});
+//Функция отображения PopUp
+function PopUpShow2(){
+    $("#popup-service-card-2").show();
+}
+//Функция скрытия PopUp
+function PopUpHide2(){
+    $("#popup-service-card-2").hide();
+}
+
+
+
+
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide3();
+});
+//Функция отображения PopUp
+function PopUpShow3(){
+    $("#popup-service-card-3").show();
+}
+//Функция скрытия PopUp
+function PopUpHide3(){
+    $("#popup-service-card-3").hide();
 }
